c408c3a954a81e4aa28e34874ff642e0b9e17899
supea <alpo.panula@gmail.com>
2021-08-02 10:35:49 +0300

    initial grid box

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+const gridBoxStyleDead = {
+  borderStyle: 'solid',
+  borderWidth: 1,
+  height: 12,
+  width: 12,
+}
+
+const gridBoxStyleAlive = {
+  ...gridBoxStyleDead,
+   backgroundColor: 'black',
+}
+
+interface P { isAlive?: boolean }
+
+const GridBox = ({ isAlive }: P) => (
+  isAlive 
+    ? <div style={gridBoxStyleAlive} />
+    : <div style={gridBoxStyleDead} /> 
+) 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <GridBox isAlive/>
   );
 }
 
